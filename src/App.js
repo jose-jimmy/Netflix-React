f8d75fa9240b5b61560634db7d7f8e019b195eca
Jose Jimmy <josejimmy8055@gmail.com>
2022-01-14 23:00:28 +0530

    Netflix-React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import NavBar from './components/NavBar/NavBar';
+import {originals,action,trending,ComedyMovies,HorrorMovies,RomanceMovies, Documentaries} from './urls'
+import './App.css'
+import Banner from './components/Banner/Banner';
+import RowPost from './components/RowPost/RowPost';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar/>
+      <Banner/>
+      <RowPost  url={originals}title='Netflix Orginals'/>
+      <RowPost url={trending}title='Trending' isSmall />
+      <RowPost url={action}title='Action' isSmall />
+      <RowPost url={ComedyMovies}title='Comedy' isSmall />
+      <RowPost url={HorrorMovies}title='Horror' isSmall />
+      <RowPost url={RomanceMovies}title='Romance' isSmall />
+      <RowPost url={Documentaries}title='Documentaries' isSmall />
+
+
+
+
+      
     </div>
   );
 }
---
+++ src/constants/constants.js
@@ -0,0 +1,3 @@
+export const baseUrl = 'https://api.themoviedb.org/3';
+export const API_KEY = "f3632717daf76ccdd177d0d38b82c45a";
+export const imageUrl='https://image.tmdb.org/t/p/original'
