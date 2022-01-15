ab7e5e797a762d8c14f4cf5745302547cd119b0c
Jose Jimmy <josejimmy8055@gmail.com>
2022-01-15 11:08:23 +0530

    Netflix-React

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -6,8 +6,7 @@ function Banner() {
    const [movie, setMovie] = useState()
         useEffect(() => {
             axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`).then((response)=>{
-               console.log(response.data.results[0]) 
-               setMovie(response.data.results[4])
+               setMovie(response.data.results[3])
             })
 
         }, [])
--- src/components/RowPost/RowPost.js
+++ src/components/RowPost/RowPost.js
@@ -41,10 +41,10 @@ function RowPost(props) {
     return (
         <div className='row'>
            <h2>{props.title}</h2>
+
            <div className='posters'>
                {movies.map((obj)=>
-
-                 <img onClick={()=>handleMovie(obj.id)}className={props.isSmall?'smallPoster' :'poster'} src= {`${imageUrl+obj.backdrop_path}`} alt="poster" />
+                 <img onClick={()=>handleMovie(obj.id)}className={props.isSmall?'smallPoster' : 'poster'} src= {`${imageUrl+obj.backdrop_path}`} alt="poster" />
                )}
            
             
